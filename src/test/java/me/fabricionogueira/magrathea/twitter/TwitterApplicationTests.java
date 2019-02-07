4464b0f0ad0314fea5f916c7cd376c358d4af854
Fabricio Nogueira <fabricio.santos@treasy.com.br>
2019-02-07 10:51:03 -0200

    feat(project): First commit
    :dart: Tech research

---
+++ src/test/java/me/fabricionogueira/magrathea/twitter/TwitterApplicationTests.java
@@ -0,0 +1,17 @@
+package me.fabricionogueira.magrathea.twitter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwitterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
