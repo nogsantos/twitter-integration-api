2211556bad16253c13aa8723369737fe595610b4
Fabricio Nogueira <nogsantos@gmail.com>
2019-02-08 09:30:41 -0200

    feat(twitter): Query
    POC to twitter queries

--- src/main/java/me/fabricionogueira/magrathea/twitter/modules/twitter/TwitterController.java
+++ src/main/java/me/fabricionogueira/magrathea/twitter/modules/twitter/TwitterController.java
@@ -1,13 +1,11 @@
 package me.fabricionogueira.magrathea.twitter.modules.twitter;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import twitter4j.TwitterException;
 
@@ -27,10 +25,14 @@ public class TwitterController {
     @ApiResponses({
             @ApiResponse(code = 200, message = "Successfully sent"),
             @ApiResponse(code = 400, message = "Processing request error"),
-            @ApiResponse(code = 404, message = "Not found")
+            @ApiResponse(code = 404, message = "Request no found")
     })
     @GetMapping("/search")
-    public ResponseEntity search() throws TwitterException {
-        return ResponseEntity.ok(service.searchtweets());
+    public ResponseEntity search(
+            @RequestParam(required = true)
+            @ApiParam(value = "Serch by hash tags") String hashTag
+    ) throws TwitterException {
+
+        return ResponseEntity.ok(service.searchTweetsBy(hashTag));
     }
 }
