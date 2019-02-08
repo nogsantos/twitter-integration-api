ab0c3f9294d9e8c5087b86d59cc03934a01564a7
Fabricio Nogueira <nogsantos@gmail.com>
2019-02-08 09:30:15 -0200

    feat(env): Twitter4j auth env param

--- setup/create.sh
+++ setup/create.sh
@@ -3,18 +3,19 @@
 function create_default_app_env() {
 cat > .env <<EOF
 DATABASE_URI=to-be-defined
-APPLICATION_NAME=$1
+APPLICATION_NAME=$1to-be-defined
 APPLICATION_CODE_VERSION=0.1.0
 SERVER_PORT=8081
-APPLICATION_DESCRIPTION=$1 API
+APPLICATION_DESCRIPTION=$1API
 APPLICATION_VERSION=v1
-CONTACT_NAME="Fabricio Nogueira"
+CONTACT_NAME=Fabricio-Nogueira
 CONTACT_SITE=http://fabricionogueira.me
 CONTACT_EMAIL=nogsantos@gmail.com
-TWITTER_CONSUMER_KEY=to-be-defined
-TWITTER_CONSUMER_SECRET=to-be-defined
-TWITTER_ACCESS_TOKEN=to-be-defined
-TWITTER_ACCESS_TOKEN_SECRET=to-be-defined
+twitter4j.oauth.consumerKey=to-be-defined
+twitter4j.oauth.consumerSecret=to-be-defined
+twitter4j.oauth.accessToken=to-be-defined
+twitter4j.oauth.accessTokenSecret=to-be-defined
+twitter4j.debug=true
 EOF
 	printf ".env file created. \n\n"
 }
