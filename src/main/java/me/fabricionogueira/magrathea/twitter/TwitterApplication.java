0fc043e09ceff139e06a9970634676b6b6b9c5c4
Fabricio Nogueira <nogsantos@gmail.com>
2019-02-11 22:10:34 -0200

    feat(mongo): Setup

--- src/main/java/me/fabricionogueira/magrathea/twitter/TwitterApplication.java
+++ src/main/java/me/fabricionogueira/magrathea/twitter/TwitterApplication.java
@@ -2,7 +2,11 @@ package me.fabricionogueira.magrathea.twitter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
+@EnableMongoAuditing
+@EnableReactiveMongoRepositories
 @SpringBootApplication(scanBasePackages = {"me.fabricionogueira.magrathea.twitter"})
 public class TwitterApplication {
 
